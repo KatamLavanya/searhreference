739517e75e3102c0b6e5425ab39cc34d56790ce5
lavan <lavan@KatamLavanya>
2024-06-24 16:05:13 +0530

    Commit Changes1

---
+++ searchreference/src/main/java/com/example/search/SearchreferenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchreferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchreferenceApplication.class, args);
+	}
+
+}
